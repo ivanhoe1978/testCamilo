a95c41dd52520b36052e59fc8425cc29ac501ba1
Iván Cordero <icordero@consultoriaglobal.com.ar>
2025-01-24 22:08:13 -0500

    fist upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, FormsModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  name:any='Camilo';
+  constructor() {
+
+  }
+  title = 'testCamilo';
+  getName(){
+    return this.name;
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,10 @@
+
+import { NgModule } from '@angular/core';
+
+import { BrowserModule } from '@angular/platform-browser';
+
+import { FormsModule } from '@angular/forms'; // Add this line
+
+
+
+import { AppComponent } from './app.component';
