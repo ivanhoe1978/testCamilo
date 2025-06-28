8037a6e75d1b2513142bd254494b334dcb7e52da
Iván Cordero <icordero@consultoriaglobal.com.ar>
2025-06-27 20:13:44 -0500

    json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,12 +10,43 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  name:any='Camilo';
+  name:string ='Camilo';
   constructor() {
 
   }
   title = 'testCamilo';
+  persona:any  = 
+  {
+    name: "Camilo",
+    age: 30,
+    email: "vero@test.com",
+    address: {
+      street: "Calle 123",
+      city: "Bogotá",
+      country: "Colombia"
+    },
+    hobbies: ["reading", "traveling", "coding"],
+    family:[
+      {
+        name: "Vero",
+        relation: "wife",
+        age: 28,
+        isHappy: true
+      },
+      {
+        name: "Sofi",
+        relation: "daughter",
+        age: 5
+      }
+    ]
+  }
+
   getName(){
+    let datito="hola";
+    console.log("HOBBIES",this.persona.hobbies[0]);
+    this.persona.hobbies[0] = "new hobby";
+    console.log("HOBBIES",this.persona.hobbies[0]);
+    /*console.log("Family",this.persona.family[0]);*/
     return this.name;
   }
 }
